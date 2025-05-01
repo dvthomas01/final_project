8e7daf63265d79180eb389a3ef7e1b936b1fb220
Evan Harrison <empir@Even.mit.edu>
2025-05-01 12:31:35 -0400

    Robot main update

--- src/robot/robot_main.cpp
+++ src/robot/robot_main.cpp
@@ -5,42 +5,45 @@
 #include "robot_motion_control.h"
 #include "RobotFunctions.h"
 
+int previousCommand = 0;
+float currentYaw = 0;
+float initialYaw = 0;
+extern struct euler_t {
+    float yaw;
+    float pitch;
+    float roll;
+} ypr;
+
+ 
 void setup() {
     
     setupIMU();
     setupDrive();
+    readIMU(false);
+    Serial1.begin(115200, SERIAL_8N1, 17, 18); // RX=17 TX=18 – change if needed
+    Serial.println("ESP32-S3 ready");
 
-    rotate(1);
 }
 
 void loop() {
-    // Send and print robot values at 20Hz
-
-    /*
-    EVERY_N_MILLIS(10000) {
-        straightline();
-        Serial.println("Hi");
+    readIMU(false);
+    currentYaw = ypr.yaw;
+    jetsonOutput output = jetsonComms();
+    if (previousCommand != output.COMMAND) initialYaw = ypr.yaw;
+    switch (output.COMMAND)
+    {
+        case 0:
+        break;
+        case 1:
+        rotate(initialYaw, currentYaw, output.INPUT_VAL);
+        break;
+        case 2:
+        break;
+        default:
     }
-    */
 
-    // EVERY_N_MILLIS(10000) {
-    //     straightline();
-    //     Serial.println("Hi");
-    // }
-
-    // Update PID at 200Hz
+    // Update PID at 1Hz
     EVERY_N_MILLIS(1000) {
        updatePIDs();
-    }
-
-    // Send and print robot values at 20Hz
-    EVERY_N_MILLIS(50) {
-        /* updateOdometry();
-    /*
-        sendRobotData();
-
-        Serial.printf("x: %.2f, y: %.2f, theta: %.2f\n",
-                    robotMessage.x, robotMessage.y, robotMessage.theta);
-    */
     }
 }
