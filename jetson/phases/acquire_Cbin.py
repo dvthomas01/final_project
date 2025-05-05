fc9a5b78c3d518502a481d245407d0df470bdffe
IdunWannut <pricestaylor@outlook.com>
2025-05-05 17:48:04 -0400

    Update acquire_Cbin.py

--- jetson/phases/acquire_Cbin.py
+++ jetson/phases/acquire_Cbin.py
@@ -12,7 +12,7 @@ class AcquireCBin:
     'ROTATE_DONE', then hand control back to the Controller so it can
     switch to Phase.FINISH (or whatever next phase you prefer).
     """
- 
+
     def __init__(self, link: SerialLink):
         self._link = link
         self._sent = False          # True after we’ve issued the rotate
@@ -30,7 +30,7 @@ class AcquireCBin:
         self._step = 0
     def read_latest_pose(self,path="pose_values.txt"):
         print("read_latest_pose")
-        try: 
+        try:
     	    with open(path, "r", encoding="utf-8") as f:
     	        vals = [float(x) for x in f.readline().strip().split()]
     	        #print (vals)
@@ -50,109 +50,109 @@ class AcquireCBin:
             pose = self.read_latest_pose()
             print ("hello from tick step 0")
             #print (pose)
-            if pose is None: 
+            if pose is None:
             	pose = [0, 0, 9999, 0]
             dist = pose[2]
             print (dist)
-            if abs(dist) < 0.05: 
+            if abs(dist) < 0.1:
                 #print ("hell from abs")
-                self._link.enqueue(Command.STOP.value) 
+                self._link.enqueue(Command.STOP.value)
                 self._step = 1
                 self._sent = True
                 return None
-            if dist > 0: 
+            if dist > 0:
                 #print ("hello from Align F")
                 self._link.enqueue(Command.ALIGN_F.value)
                 self._sent = True
-            else: 
+            else:
                 #print ("hello from Align B")
                 self._link.enqueue(Command.ALIGN_B.value)
                 self._sent = True
             print("acquire_Cbin.py STEP 1. DIST TO 9: ", dist)
             return None
-        if self._step == 1: 
-        
-            self._link.enqueue(Command.STOP.value)  
+        if self._step == 1:
+
+            self._link.enqueue(Command.STOP.value)
             self._step = 2
             self._sent = True
             print("acquire_Cbin.py STEP 2")
 
             return None
-        
-        
+
+
         if self._step == 2 and event and event.name == "STOP":
-            self._link.enqueue(Command.ROTATE_CCW.value) 
+            self._link.enqueue(Command.ROTATE_CCW.value)
             self._sent = True
             self._step = 3
             print("acquire_Cbin.py STEP 3")
             return None
-        
+
         if self._step == 3 and event and event.name == "ROTATE_DONE":
-            self._link.enqueue(Command.STOP.value) 
+            self._link.enqueue(Command.STOP.value)
             self._sent = True
             self._step = 4
             print("acquire_Cbin.py STEP 4")
             return None
-        
+
         if self._step == 4 and event:
-            self._link.enqueue(Command.APPROACH_PICKUP.value) 
+            self._link.enqueue(Command.APPROACH_PICKUP.value)
             pose = self.read_latest_pose()
             #print (pose)
-            if pose is None: 
+            if pose is None:
             	return None
             dist = pose[1]
             if dist<0.5:
             #if time.monotonic() - self._t0 >= self._driveduration:
-                self._link.enqueue(Command.STOP.value) 
+                self._link.enqueue(Command.STOP.value)
                 self._step = 5
                 self._sent = True
                 return None
             self._sent = True
             print("setup.py STEP 5")
-            return None   
-        
-        if self._step == 5: 
-            self._link.enqueue(Command.STOP.value)  
+            return None
+
+        if self._step == 5:
+            self._link.enqueue(Command.STOP.value)
             self._step = 6
             self._sent = True
             print("setup.py STEP 6")
             return None
-        
+
         if self._step == 6 and event and event.name == "STOP":
-            self._link.enqueue(Command.GRAB_BIN.value)  
+            self._link.enqueue(Command.GRAB_BIN.value)
             self._step = 7
             self._sent = True
             print("setup.py STEP 7")
-	
-        
-        if self._step == 7 and event and event.name =="BIN_GRABBED": #TODO: is this correct. update the success of grab 
-    
-        	
+
+
+        if self._step == 7 and event and event.name =="BIN_GRABBED": #TODO: is this correct. update the success of grab
+
+
             pose = self.read_latest_pose()
             print ("hello from tick step 7")
             #print (pose)
-            if pose is None: 
+            if pose is None:
             	pose = [0, 0, -9999, 0]
             dist = pose[2]
             print (dist)
-            if abs (dist)< 0.05 : 
-                self._link.enqueue(Command.STOP.value) 
+            if abs (dist)< 0.05 :
+                self._link.enqueue(Command.STOP.value)
                 self._step = 8
                 self._sent = True
                 print("acquire_Cbin.py STEP 8")
                 return None
-            if (dist)>0: 
+            if (dist)>0:
                 self._link.enqueue(Command.ALIGN_F.value)
                 self._sent = True
                 return None
-            else: 
+            else:
                 self._link.enqueue(Command.ALIGN_B.value)
                 self._sent = True
                 return None
 
-        
+
         if self._step == 8 and event and event.name == "STOP":
-            self._link.enqueue(Command.ROTATE_CCW.value) 
+            self._link.enqueue(Command.ROTATE_CCW.value)
             self._sent = True
             self._step = 9
             print("acquire_Cbin.py STEP 9")
@@ -161,31 +161,30 @@ class AcquireCBin:
         if self._step ==  9 and event and event.name == "ROTATE_DONE":
             pose = self.read_latest_pose()
             dist = pose[1]
-            self._link.enqueue(Command.APPROACH_PICKUP.value) 
+            self._link.enqueue(Command.APPROACH_PICKUP.value)
             if dist < 0.5:
             #if time.monotonic() - self._t0 >= self._driveduration:
-                self._link.enqueue(Command.STOP.value) 
+                self._link.enqueue(Command.STOP.value)
                 self._step = 10
                 self._sent = True
                 self._t0 = time.monotonic()
                 return None
             self._sent = True
             print("setup.py STEP 10")
-            return None   
-        
-        
-        if self._step ==  10 and event: 
-            self._link.enqueue(Command.DEPOSIT_BIN.value) 
+            return None
+
+
+        if self._step ==  10 and event:
+            self._link.enqueue(Command.DEPOSIT_BIN.value)
             if time.monotonic() - self._t0 >= self._depositduration:
-                self._link.enqueue(Command.STOP.value) 
+                self._link.enqueue(Command.STOP.value)
                 self._step = 11
                 self._sent = True
                 return None
             self._sent = True
             print("setup.py STEP 11")
-            return None   
+            return None
 
-        
+
         if self._step == 11 and event and event.name == "STOP":
-            return Phase.FINISH        
-
+            return Phase.FINISH
