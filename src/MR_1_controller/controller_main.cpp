b79e82284ff482e494e46c50627d1f155945aba6
aladd2 <avaladd2@mit.edu>
2025-05-09 22:43:28 -0400

    rename environments

---
+++ src/MR_1_controller/controller_main.cpp
@@ -0,0 +1,65 @@
+#include <Bounce2.h>
+#include "wireless.h"
+#include "util.h"
+#include "joystick.h"
+#include "dpad.h"
+#include "display.h"
+#include "controller_pinout.h"
+
+#define BOUNCE_PIN1 8
+#define BOUNCE_PIN2 6
+#define BOUNCE_PIN3 12
+
+Bounce bounceF = Bounce(); //button to spin flywheels forward
+Bounce bounceR = Bounce(); //button to spin flywheels reverse
+Bounce bounceI = Bounce(); //button to interrupt autonomy with joystick control
+
+int dif = 1; //forward
+int dir = 1; //reverse
+int dii = 1; //interrupt
+
+
+ControllerMessage prevControllerMessage;
+
+Joystick joystick1(JOYSTICK1_X_PIN, JOYSTICK1_Y_PIN);
+//Joystick joystick2(JOYSTICK2_X_PIN, JOYSTICK2_Y_PIN);
+
+void setup() {
+    Serial.begin(115200);
+
+    setupWireless();
+
+    joystick1.setup();
+
+    bounceF.attach(BOUNCE_PIN1, INPUT_PULLUP);
+    bounceR.attach(BOUNCE_PIN2, INPUT_PULLUP);
+    bounceI.attach(BOUNCE_PIN3, INPUT_PULLUP);
+
+    Serial.println("Setup complete.");
+}
+
+void loop() {
+    // Read and send controller sensors
+    EVERY_N_MILLIS(20) {
+
+        bounceF.update();
+        bounceR.update();
+        bounceI.update();
+
+        dif = bounceF.read();
+        dir = bounceR.read();
+        dii = bounceI.read();
+
+        controllerMessage.millis = millis();
+        controllerMessage.joystick1 = joystick1.read();
+        controllerMessage.debouncedInputF = dif;
+        controllerMessage.debouncedInputR = dir;
+        controllerMessage.debouncedInterrupt = dii;
+
+        
+        if (!(prevControllerMessage == controllerMessage)) {
+            sendControllerData();
+            prevControllerMessage = controllerMessage;
+        }
+    }
+}
---
+++ src/MR_1_robot/robot_drive.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include "robot_pinout.h"
+#include "MotorDriver.h"
+#include "PID.h"
+#include "EncoderVelocity.h"
+#include "robot_drive.h"
+#include "robot_motion_control.h"
+#include "RobotFunctions.h"
+
+
+MotorDriver motors[NUM_MOTORS] = { {A_DIR1, A_PWM1, 0}, {A_DIR2, A_PWM2, 1},
+                                   {B_DIR1, B_PWM1, 2}, {B_DIR2, B_PWM2, 3} };
+
+
+// NUM_MOTORS != NUM_ENCODERS, See comment in robot_drive.h
+EncoderVelocity encoders[NUM_ENCODERS] = { {ENCODER1_B_PIN, ENCODER1_A_PIN, CPR_312_RPM, 0.2}, 
+                                         {ENCODER2_B_PIN, ENCODER2_A_PIN, CPR_312_RPM, 0.2},  
+                                         };
+
+PID pids[NUM_ENCODERS] = { {Kp, Ki, Kd, 0, pidTau, false}, {Kp, Ki, Kd, 0, pidTau, false} };
+
+
+double setpoints[NUM_MOTORS] = {0, 0, 0, 0};
+double velocities[NUM_ENCODERS] = {0, 0};
+double controlEfforts[NUM_MOTORS] = {0, 0, 0, 0};
+
+void setupDrive() 
+{
+    for (uint8_t i = 0; i < 4; i++)
+        motors[i].setup();
+}
+
+void updateDriveSetpoints(double left, double right) {
+    setpoints[2] = left;
+    setpoints[3] = right;
+}
+
+void updateFlywheelSetpoints(double left, double right) {
+    setpoints[0] = left; //left flywheel
+    setpoints[1] = right; //right flywheel
+}
+
+void updatePIDs() {
+    for (uint8_t i = 0; i < NUM_MOTORS; i++) {
+        // Drive motors: 2 & 3
+        // Flywheels: 0 & 1
+        // There are only encoders for 2 & 3
+        if (i > 1) {
+            velocities[i - 2] = pow(-1, i - 2) * encoders[i - 2].getVelocity();
+            controlEfforts[i] = pids[   i - 2].calculateParallel(velocities[i - 2], setpoints[i]);
+        } else {
+            controlEfforts[i] = setpoints[i];
+        }
+
+        motors[i].drive(controlEfforts[i]);
+    } 
+}
